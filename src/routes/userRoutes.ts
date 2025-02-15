be7bbe6f47ee48deac937ba1e7025cbf42d24590
dara01x <daramistefa30@gmail.com>
2025-02-16 00:05:22 +0300

    Add user routes for CRUD operations

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { UserController } from "../controllers/userController";
+import { UserService } from "../services/userService";
+
+const router = Router();
+const userService = new UserService();
+const userController = new UserController(userService);
+
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserById);
+router.post("/", userController.createUser);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
+
+export default router;
