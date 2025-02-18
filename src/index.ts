c3b6a851e9efa4e3f02d537995fe482db5b2c699
dara01x <daramistefa30@gmail.com>
2025-02-18 16:05:47 +0300

    Refactor index.ts to improve route organization and add user routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import  cors from "cors";
+import cors from "cors";
 import * as dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes";
+import { error } from "console";
 
 dotenv.config();
 
@@ -10,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
-
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
